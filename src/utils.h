f1c85caa538f4d8df7b9f6429e1f3e46e66de539
Doncho N. Gunchev <dgunchev@gmail.com>
2022-12-03 00:18:07 +0200

    Reformat a little.
    Signed-off-by: Doncho N. Gunchev <dgunchev@gmail.com>

--- src/utils.h
+++ src/utils.h
@@ -2,7 +2,7 @@
 
 /// GCD - Greatest common divisor with two arguments
 template <typename T>
-constexpr T gcd(const T& x, const T& y) noexcept {
+constexpr T gcd(const T &x, const T &y) noexcept {
     if (y == 0) {
         return x;
     }
@@ -15,23 +15,23 @@ static_assert(gcd(1071, 462) == 21, "Error");
 
 /// GCD - Greatest common divisor with more than two arguments
 template <typename T, typename... Args>
-constexpr T gcd(const T& x, const T& y, Args ... args ) {
+constexpr T gcd(const T &x, const T &y, Args... args) {
     return gcd(gcd(x, y), args...);
 }
 
-// Sort two numbers without using temporary (triple XOR)
+/// Sort two numbers without using temporary (triple XOR)
 template <typename T>
-inline constexpr void sort2(T& a, T& b) noexcept {
-    if (a > b) {  // fancy swap, saves the memory for one temporary
+inline constexpr void sort2(T &a, T &b) noexcept {
+    if (a > b) { // fancy swap, saves the memory for one temporary
         a ^= b;
         b ^= a;
         a ^= b;
     }
 }
 
-// Sort three numbers without using temporary (triple XOR * 3)
+/// Sort three numbers without using temporary (triple XOR * 3)
 template <typename T>
-inline constexpr void sort3(T& a, T& b, T& c) noexcept {
+inline constexpr void sort3(T &a, T &b, T &c) noexcept {
     sort2(a, b);
     sort2(b, c);
     sort2(a, b);
--- src/water.cpp
+++ src/water.cpp
@@ -12,6 +12,11 @@
 
 using water = uint16_t; // Water level measurement
 
+template <typename T>
+constexpr T type_max() noexcept {
+    return std::numeric_limits<water>::max();
+}
+
 /// Three water vessel's current contents in liters of water, the volumes must be kept in one more State variable.
 // Inherits comparison operators from std::array<T, S>()
 class VesselsState: public std::array<water, 3> {
@@ -20,14 +25,12 @@ public:
     constexpr VesselsState(water a, water b, water c) noexcept: std::array<water, 3>({a, b, c}) {}
 
     /// Hash for unordered containers, c++ 23 it can even be static (__cpp_static_call_operator, P1169R3)
-    constexpr size_t operator()(const VesselsState& state) const noexcept {
-        return (size_t(state[0])*std::numeric_limits<water>::max()
-                + state[1])*std::numeric_limits<water>::max()
-                + state[2];
+    constexpr size_t operator()(const VesselsState &state) const noexcept {
+        return (size_t(state[0]) * type_max<water>() + state[1]) * type_max<water>() + state[2];
     };
 
     /// Return new state after transferring water
-    VesselsState transfer(unsigned from, unsigned to, const VesselsState& volumes) const noexcept {
+    VesselsState transfer(unsigned from, unsigned to, const VesselsState &volumes) const noexcept {
         VesselsState result = *this; // copy
         const water dst_free = volumes.at(to) - this->at(to);
         if (this->at(from) <= dst_free) {
@@ -41,7 +44,7 @@ public:
     }
 
     /// Calculate all possible next states
-    std::vector<VesselsState> next_states(const VesselsState& volumes) const {
+    std::vector<VesselsState> next_states(const VesselsState &volumes) const {
         std::vector<VesselsState> result;
         result.reserve(12); // up to 12, use only 1 memory allocation
 
@@ -80,13 +83,13 @@ public:
 
 // "Unit test" for C++20 and above
 #if __cplusplus >= 202002L
-static_assert(VesselsState{1,2,3} == VesselsState{1,2,3});
-static_assert(VesselsState{1,2,3} != VesselsState{2,2,3});
-static_assert(VesselsState{2,2,3} != VesselsState{1,2,3});
-static_assert(VesselsState{1,2,8} != VesselsState{1,2,3});
-static_assert(VesselsState{1,2,3} != VesselsState{1,2,8});
-static_assert(VesselsState{1,2,3} < VesselsState{1,2,4});
-static_assert(VesselsState{2,2,3} > VesselsState{1,2,4});
+static_assert(VesselsState{1, 2, 3} == VesselsState{1, 2, 3});
+static_assert(VesselsState{1, 2, 3} != VesselsState{2, 2, 3});
+static_assert(VesselsState{2, 2, 3} != VesselsState{1, 2, 3});
+static_assert(VesselsState{1, 2, 8} != VesselsState{1, 2, 3});
+static_assert(VesselsState{1, 2, 3} != VesselsState{1, 2, 8});
+static_assert(VesselsState{1, 2, 3} < VesselsState{1, 2, 4});
+static_assert(VesselsState{2, 2, 3} > VesselsState{1, 2, 4});
 #endif
 
 /// Solve the water pouring puzzle with tap, sink and empty initial state.
@@ -105,8 +108,7 @@ protected:
     Visited m_visited{}; // States visited
 
 public:
-    explicit WaterPouringPuzzleSolver(const VesselsState& volumes): m_volumes(volumes) {
-    }
+    explicit WaterPouringPuzzleSolver(const VesselsState &volumes): m_volumes(volumes) {}
 
     /// Returns in how many steps it can be solved (and prints it), -1 is no solution
     int solve_water(const water target) {
@@ -115,13 +117,13 @@ public:
             return 0;
         }
 
-        init();  // Allow the method to be called multiple times, optimize the number of memory allocations
+        init(); // Allow the method to be called multiple times, optimize the number of memory allocations
 
-        m_visited.emplace(0, 0, 0); // We don't want to empty all of them
+        m_visited.emplace(0, 0, 0);  // We don't want to empty all of them
         m_visited.insert(m_volumes); // We also don't want to fill all of them
 
-        int step = 0; // count steps
-        size_t old_ptr = 0; // All elements [0 .. history.size()) are new
+        int step = 0;                                               // count steps
+        size_t old_ptr = 0;                                         // All elements [0 .. history.size()) are new
         m_history.emplace_back(VesselsState{0, 0, 0}, INVALID_IDX); // Initial state
 
         while (old_ptr != m_history.size()) {
@@ -129,9 +131,9 @@ public:
 
             size_t next_ptr = m_history.size();
             for (size_t p = old_ptr; p < next_ptr; ++p) {
-                const VesselsState& old_state = m_history.at(p).first;
+                const VesselsState &old_state = m_history.at(p).first;
 
-                for (VesselsState new_state: old_state.next_states(m_volumes)) {
+                for (VesselsState new_state : old_state.next_states(m_volumes)) {
                     if (m_visited.find(new_state) != m_visited.end()) {
                         continue;
                     }
@@ -148,7 +150,7 @@ public:
             old_ptr = next_ptr;
         }
 
-        return -1;  // No new state transitions possible, no solution
+        return -1; // No new state transitions possible, no solution
     }
 
 protected:
@@ -168,20 +170,21 @@ protected:
         }
         assert(m_history.size() != 0);
 
-        fmt::print("Solved measure {} liters of water using {}, {} and {} vessels in {} steps\n",
-                target, m_volumes.at(0), m_volumes.at(1), m_volumes.at(2), steps);
+        fmt::print("Solved measure {} liters of water using {}, {} and {} vessels in {} steps\n", target,
+                   m_volumes.at(0), m_volumes.at(1), m_volumes.at(2), steps);
         fmt::print("┌──────┬─────┬─────┬─────┐\n");
         fmt::print("│ Step │ {: >3} │ {: >3} │ {: >3} │\n", m_volumes.at(0), m_volumes.at(1), m_volumes.at(2));
         fmt::print("├──────┼─────┼─────┼─────┤\n");
 
-        // If only the first solution is needed we can modify the history to reverse the index pointers and walk forward.
+        // If only the first solution is needed we can modify the history to reverse the index pointers and walk
+        // forward.
 
         std::vector<int> solution;
         solution.resize(static_cast<size_t>(steps) + 1);
 
         int history_idx = static_cast<int>(m_history.size() - 1);
         for (int pos = steps; pos != -1; --pos) {
-            solution.at(static_cast<size_t>(pos)) = history_idx; // Save current
+            solution.at(static_cast<size_t>(pos)) = history_idx;                 // Save current
             history_idx = m_history.at(static_cast<size_t>(history_idx)).second; // travel back
 
             if (pos == 0) {
@@ -192,15 +195,14 @@ protected:
         };
 
         for (int i = 0; i != steps + 1; ++i) {
-            const VesselsState& state = m_history.at(static_cast<size_t>(solution.at(static_cast<size_t>(i)))).first;
-            fmt::print("│ {: >3}. │ {: >3} │ {: >3} │ {: >3} │\n",
-                    i, state.at(0), state.at(1), state.at(2));
+            const VesselsState &state = m_history.at(static_cast<size_t>(solution.at(static_cast<size_t>(i)))).first;
+            fmt::print("│ {: >3}. │ {: >3} │ {: >3} │ {: >3} │\n", i, state.at(0), state.at(1), state.at(2));
         }
         fmt::print("└──────┴─────┴─────┴─────┘\n");
     }
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 5) {
         puts("Solve the three water vessels, tap and sink problem.\n\n"
              "Usage:\n\twater LIMIT_1 LIMIT_2 LIMIT_3 TARGET\n\n"
